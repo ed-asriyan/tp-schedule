85183a343f196ffe68fadb12393f913c2f0accbc
Ed Asriyan <ed-asriyan@protonmail.com>
2017-11-07 04:13:32 +0300

    Added task description

--- app/src/main/java/ru/mail/park/tpschedule/MainActivity.java
+++ app/src/main/java/ru/mail/park/tpschedule/MainActivity.java
@@ -3,6 +3,14 @@ package ru.mail.park.tpschedule;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+ * TODO 1) implement logic for network manager to handle updates greatly
+ * TODO 2) consider restructuring database
+ * TODO 3) think on how to make MapBuilder templated
+ * TODO 4) implement transport facade
+ * TODO 5) test for work
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
